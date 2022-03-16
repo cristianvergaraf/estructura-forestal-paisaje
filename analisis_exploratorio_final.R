0122a833f28f2f437b1a311caef6ec17fc750b8a
cvergaraf <cristianvergaraf@gmail.com>
2022-03-16 16:38:07 +0100

    exploratory data

--- analisis_exploratorio_final.R
+++ analisis_exploratorio_final.R
@@ -1,4 +1,8 @@
 ###
+### Este script tiene como objetivo realizar un análisis exploratorio 
+### De un conjunto de datos multivariante.
+
+# Cargamos las paquetes necesarios
 
 library(readr)
 library(dplyr)
@@ -6,57 +10,104 @@ library(ggplot2)
 library(corrplot)
 library(GGally)
 
+# Directorio de trabajo
+
 setwd("~/GitHub/estructura-forestal-paisaje")
 
+# Cargamos los datos desde un archivo csv.
 pl_metricas<- read.csv("metricas_seleccion_ejercicio.csv")
 
-X = pl_metricas[3:18]
+# Seleccionamos solo las filas númericas
+
+X <- pl_metricas[3:18]
+
+# configuramos la ventana para hacer los gráficos, vamos agrupar 
+# en ventanas de dos por dos.
 
 par(mfrow = c(2,2))
 
+# Una primera aproximación para conocer la distribución de las variables es un 
+# Boxplot por variable
+
 sapply(seq(1,16),function(j)boxplot(X[,j],main=colnames(X)[j],xlab="",col="yellow"))
 
+# En un segundo paso generamos un histrograma por variable
 
 par(mfrow=c(2,4))
 sapply(seq(1,16),function(j)hist(X[,j],main=colnames(X)[j],xlab="",col="blue",breaks = "Sturges"))
 
+## En tercer lugar podemos representar distribución de los datos
+## de forma continua con una función de densidad.
 
 par(mfrow=c(2,4))
 sapply(seq(1,16),function(j)plot(density(X[,j],kernel="gaussian"),main=colnames(X)[j],xlab="",col="blue",lwd=2))
 
+## Relaciones entre variables se pueden evaluar visualmente 
+## a través de histogramas
 
 pairs(X[1:5],pch=19,col="black",
       lower.panel = panel.smooth)
 
+pairs(X[6:10],pch=19,col="black",
+      lower.panel = panel.smooth)
+
+## Adicionalmente podemos calcular la correlación entre variables
+
 pl_metricas.cor <- cor(X, method = "pearson")
-pl_round.cor <- round(pl_metricas.cor, digits = 1)
+pl_round.cor <- round(pl_metricas.cor, digits = 2) # con esta línea dejamos solo dos dígitos para el resultado
 
-pl_metricas.cor
-
+# Definimos el marco para los gráficos
 par(mfrow = c(1,1))
 
+# Definimos una escala de colores
 col <- colorRampPalette(c("#BB4444", "#EE9988", "#FFFFFF",
                           "#77AADD", "#4477AA"))
+
+## Podemos represesntar las correlaciones por medio de un gráfico
 
 corrplot(pl_round.cor, method = "shade", sade.col = NA, 
          tl.col = "black", tl.srt = 45,
          col = col(200), addCoef.col = "black", addcolorlabel = "no",
-         order = "AOE", number.digits = 2, number.cex = 0.7)
-
+         order = "AOE", number.digits = 2, number.cex = 0.6)
 
 corrplot(pl_metricas.cor, method = "shade", shade.col = NA, 
          tl.col = "black", tl.srt = 45,
          col = col(200), addCoef.col = "black", addcolorlabel = "no",
          order = "AOE", type = "upper", 
-         diag = F, number.digits = 2, number.cex = 0.7)
+         diag = F, number.digits = 2, number.cex = 0.6)
 
 ######   
 # Scatterplots revisar diseno separar por variables. 
 
-ggpairs(pl_metricas)
+# Este opción permite hacer gráficos con scaterplot para entender las relaciones entre variables
 
-ggpairs(X[1:8])
-ggpairs(X[9:16])
 
-ggpairs(X[1:16])
 
+ggpairs(X, columns = c(1,4,7)) # podemos seleccionar algunas columnas del dataset
+
+
+## Podemos agregar una linea de regresión
+
+ggpairs(X, columns = c(1,4,7,8), 
+        lower = list(continuous = "smooth"))
+
+## Podemos con una función colocar colores a estas líneas y el histograma
+
+lowerFn <- function(data, mapping, method = "lm", ...){
+  p <- ggplot(data = data, mapping = mapping) +
+    geom_point(colour = "blue") + 
+    geom_smooth(method = method, color = "red", ...)
+  p
+}
+
+ggpairs(
+  X, columns = c(1,3,11), 
+  lower = list(continuous = wrap(lowerFn, method = "lm")),
+  diag = list(continuous = wrap("barDiag", colour = "blue")),
+  upper = list(continuous = wrap("cor", size = 5))
+)
+
+
+
+  
+  
