1f74f894cbfa0d56cb13a0d1b2be25cc6934c615
cvergaraf <cristianvergaraf@gmail.com>
2022-03-16 17:42:35 +0100

    escalado

--- analisis_exploratorio_final.R
+++ analisis_exploratorio_final.R
@@ -1,6 +1,6 @@
 ###
 ### Este script tiene como objetivo realizar un análisis exploratorio 
-### De un conjunto de datos multivariante.
+### De un conjunto de datos multivariantes.
 
 # Cargamos las paquetes necesarios
 
@@ -21,6 +21,54 @@ pl_metricas<- read.csv("metricas_seleccion_ejercicio.csv")
 
 X <- pl_metricas[3:18]
 
+
+########
+# Reescalado lineal de los datos
+
+library(scales)
+# En esta funcion creamos un dataframe con solo el nombre de las comunas
+# De esta manera creamos un dataframe con el largo adecuado para poder 
+# Agregar nuevas columnas
+
+rescale.many <- function(dataframe, cols){
+  dataframe1 <- data.frame(pl_metricas[1])
+  names <- names(dataframe)
+  for(col in cols){
+    name <- paste(names[col], "rescaled", sep = ".")
+    dataframe1[name] <- rescale(dataframe[,col])
+  }
+  cat(paste("Hemos rescalada ", length(cols), " variables(s)"))
+  dataframe1
+}
+
+plantaciones_rescale <- rescale.many(X, c(1:16))  
+
+View(plantaciones_rescale)
+
+#########################
+# Normalización de las variables
+
+
+pl_norm <- scale(X, center = TRUE, scale = TRUE)
+
+
+## Esta función conserva el dataframe original
+scale.many = function(dataframe, cols){
+  names <- names(dataframe)
+  for(col in cols){
+    name <- paste(names[col], "z", sep = ".")
+    dataframe[name] <- scale(dataframe[,col])
+  }
+  cat(paste("Hemos normalizado ", length(cols), "variable(s)"))
+  dataframe
+}
+
+
+############
+
+
+
+
 # configuramos la ventana para hacer los gráficos, vamos agrupar 
 # en ventanas de dos por dos.
 
@@ -34,7 +82,10 @@ sapply(seq(1,16),function(j)boxplot(X[,j],main=colnames(X)[j],xlab="",col="yello
 # En un segundo paso generamos un histrograma por variable
 
 par(mfrow=c(2,4))
+
 sapply(seq(1,16),function(j)hist(X[,j],main=colnames(X)[j],xlab="",col="blue",breaks = "Sturges"))
+sapply(seq(1,16),function(j)hist(pl_norm[,j],main=colnames(pl_norm)[j],xlab="",col="blue",breaks = "Sturges"))
+sapply(seq(2,17),function(j)hist(plantaciones_rescale[,j],main=colnames(plantaciones_rescale)[j],xlab="",col="blue",breaks = "Sturges"))
 
 ## En tercer lugar podemos representar distribución de los datos
 ## de forma continua con una función de densidad.
@@ -91,7 +142,7 @@ ggpairs(X, columns = c(1,4,7)) # podemos seleccionar algunas columnas del datase
 ggpairs(X, columns = c(1,4,7,8), 
         lower = list(continuous = "smooth"))
 
-## Podemos con una función colocar colores a estas líneas y el histograma
+## Podemos con una función colocar colores a la linea y el histograma
 
 lowerFn <- function(data, mapping, method = "lm", ...){
   p <- ggplot(data = data, mapping = mapping) +
@@ -107,7 +158,3 @@ ggpairs(
   upper = list(continuous = wrap("cor", size = 5))
 )
 
-
-
-  
-  
